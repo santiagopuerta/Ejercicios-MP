6c4214bd77c4182a8defc68a3972dd8aa064baa5
Santiago <elwarxy@gmail.com>
2012-05-31 14:07:07 +0200

    Arreglando el ejercicio 3.

--- Relaccion ficheros/src/3.cpp
+++ Relaccion ficheros/src/3.cpp
@@ -8,12 +8,22 @@ using namespace std;
 
 int main(){
 
+	char c;
 	ofstream archivoS("datos.dat"); // Archivo de salida para los datos introducidos.
 	if(!archivoS){
 		cout << "Error al abrir el fichero datos.dat." << endl;
 		return 1;
 	}
 
-	
-
+	cout << "Introduzca elementos tipo int para almacenarlos en datos.dat." << endl;
+	while((c = cin.get()) != EOF ){
+		if((c >= '0' && c <= '9') || (c == ' ')){
+			archivoS.put(c);
+		}else{
+			cout << "Introduzca solo numeros, este elemento no se guardara." << endl;
+		}
+	}
+	cout << endl;
+	cout << "Se han guardado los elementos introducidos en el fichero datos.dat." << endl;
+	archivoS.close();
 }
